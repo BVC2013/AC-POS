74ca728ccee06fbe8fda6603bb822e3888aa02d6
Bhavyansh Shivakoti <dabhavking@gmail.com>
2026-02-12 09:49:05 -0500

    ngrok

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-export const AUTOCOMPLETE_API_URL = "https://8606-98-109-121-116.ngrok-free.app";
+export const AUTOCOMPLETE_API_URL = "https://7a11-98-109-121-116.ngrok-free.app";
 export const BACKEND_API_URL = "https://ac-pos.onrender.com"; // Replace with your real Render backend URL
